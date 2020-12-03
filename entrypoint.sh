a9fe008b4258e946e1bf7f18cb9ad5dec907804c
PepperLola <PepperLola@users.noreply.github.com>
2020-12-03 11:04:34 -0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,7 @@ chmod 600 $SSH_PRIVATE_KEY_FILE
 echo 'Starting SFTP...'
 printf "%s" "put -r $5 $6" > $SFTP_BATCH_FILE
 
-sftp -b $SFTP_BATCH_FILE -P $3 -i $SSH_PRIVATE_KEY_FILE $1@$2
+sftp -b $SFTP_BATCH_FILE -P $3 -o StrictHostKeyChecking=no -i $SSH_PRIVATE_KEY_FILE $1@$2
 
 echo 'Deploy successful!'
 exit 0
