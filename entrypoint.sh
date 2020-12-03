869c0a3f648bf6bc1df6715b1c9b20ff22a4ca8b
PepperLola <PepperLola@users.noreply.github.com>
2020-12-03 12:31:38 -0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,8 @@ chmod 600 $SSH_PRIVATE_KEY_FILE
 echo 'Starting SFTP...'
 printf "%s" "put -r $5 $6" > $SFTP_BATCH_FILE
 
-ssh-keyscan -H andy.dreamhost.com >> ~/.ssh/known_hosts
+# ssh-keyscan -H andy.dreamhost.com >> ~/.ssh/known_hosts
+ssh -o StrictHostKeyChecking=no -p $3 -i $TEMP_SSH_PRIVATE_KEY_FILE $1@$2 mkdir -p $5
 
 sftp -b $SFTP_BATCH_FILE -P $3 $7 -o StrictHostKeyChecking=no -i $SSH_PRIVATE_KEY_FILE $1@$2
 
